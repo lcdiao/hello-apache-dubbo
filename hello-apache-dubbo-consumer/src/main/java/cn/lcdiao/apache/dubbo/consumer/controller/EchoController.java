bba5ce61e4d892378d962de80f74112def581f59
lcdiao <120879566@qq.com>
2019-08-30 09:48:46 +0800

    使用nacos进行外部化配置

--- hello-apache-dubbo-consumer/src/main/java/cn/lcdiao/apache/dubbo/consumer/controller/EchoController.java
+++ hello-apache-dubbo-consumer/src/main/java/cn/lcdiao/apache/dubbo/consumer/controller/EchoController.java
@@ -2,6 +2,8 @@ package cn.lcdiao.apache.dubbo.consumer.controller;
 
 import cn.lcdiao.apache.dubbo.provider.api.EchoService;
 import org.apache.dubbo.config.annotation.Reference;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,14 +12,19 @@ import org.springframework.web.bind.annotation.RestController;
  * @author diao
  * @date 2019/8/29
  */
+//根据配置文件动态刷新
+@RefreshScope
 @RestController
 public class EchoController {
 
     @Reference(version = "1.0.0")
     private EchoService echoService;
 
+    @Value("${user.name}")
+    private String username;
+
     @GetMapping(value = "/echo/{string}")
     public String echo(@PathVariable String string) {
-        return echoService.echo(string);
+        return echoService.echo(string) + " " + username;
     }
 }
